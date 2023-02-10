c3c58a5e4f6137e76c73daec3efac326ce40102a
JDev000 <jdeveloper2022@gmail.com>
2023-02-10 13:26:48 +0800

    2nd commit

---
+++ public/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("modal").style.display = "flex";
+  });
+  
+  document.getElementById("closeModal").addEventListener("click", function() {
+    document.getElementById("modal").style.display = "none";
+  });
+  
+
